35cda60bf3a60a309cb9dd070b071cdf6771d5e4
CataCimpean <catacimpean1@gmail.com>
2018-04-23 22:08:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cimpean/flowershopandroid/FlowerMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cimpean.flowershopandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlowerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flower_main);
+    }
+}
